286641c16d725e11d372e196fa9ae601abbdb2d7
ivhyarleth <arleth.lapa@utec.edu.pe>
2023-06-19 02:58:36 -0500

    proyecto2

--- backend/inverted_index.py
+++ backend/inverted_index.py
@@ -34,25 +34,11 @@ class InvertedIndex:
 
   def remove_emoji(self, text):
     emoji_pattern = re.compile("["
-      u"\U0001F600-\U0001F64F"  # emoticons
-      u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-      u"\U0001F680-\U0001F6FF"  # transport & map symbols
-      u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-      u"\U00002500-\U00002BEF"  # chinese char
-      u"\U00002702-\U000027B0"
-      u"\U00002702-\U000027B0"
-      u"\U000024C2-\U0001F251"
-      u"\U0001f926-\U0001f937"
-      u"\U00010000-\U0010ffff"
-      u"\u2640-\u2642" 
-      u"\u2600-\u2B55"
-      u"\u200d"
-      u"\u23cf"
-      u"\u23e9"
-      u"\u231a"
-      u"\ufe0f"  # dingbats
-      u"\u3030"
-                    "]+", re.UNICODE)
+      u"\U0001F600-\U0001F64F"  
+      u"\U0001F300-\U0001F5FF"  
+      u"\U0001F680-\U0001F6FF" 
+      u"\U0001F1E0-\U0001F1FF"  
+      u"\u3030" "]+", re.UNICODE)
     return re.sub(emoji_pattern, '', text)
 
   def remove_url(self, text):
@@ -62,9 +48,7 @@ class InvertedIndex:
     return text
 
   def remove_special_character(self, text):
-    special_characters = ('\"','\'','º','&','¿','?','¡','!',' “','…','👏',
-								'-','—','‘','•','›','‼','€','£','↑','→','↓','↔',
-								'↘','↪','√','∧','⊃','⌒','⌛','⏬','⏯','⏰','⏹')
+    special_characters = ('\"','\'','º','&','¿','?','¡','!',' “','…','-','—','‘','•','›','‼','€','£','↑','→','↓','↔','↘','↪','√','∧','⊃','⌒')
     for char in special_characters:
       text = text.replace(char, "")
     return text
